61ce2172147de5ce5837b63bac0a84c291c689cf
Alexandre C do Carmo <alexandrecdocarmo@Mac-mini-de-Alexandre-2.local>
2022-03-14 22:13:20 -0300

    separando os widgets em classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import './components/transaction_user.dart';
 
 main()=>runApp(ExpensesApp());
 
@@ -16,11 +15,6 @@ class ExpensesApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
 
-  final _transactions = [
-    Transaction(id: 't1', title: 'Novo Tênis de corrida', value: 310.76, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now())
-  ];
-  
   @override
   Widget build(BuildContext context){
       return Scaffold(
@@ -28,7 +22,7 @@ class MyHomePage extends StatelessWidget {
           title: Text('Despesas pessoais'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
@@ -38,58 +32,7 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-
-            Column(
-              children: _transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          )
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          tr.value.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple
-                          ),
-
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                              tr.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          Text(
-                            tr.date.toString(),
-                            style: TextStyle(
-                              color: Colors.grey
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
+            TransactionUser(),
           ],
         ),
       );
