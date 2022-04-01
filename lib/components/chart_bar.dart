d46710d8957837aee72107cdbfded19ea14daa3a
Alexandre C do Carmo <alexandrecdocarmo@Mac-mini-de-Alexandre-2.local>
2022-03-31 22:31:37 -0300

    adicionado estrutura inicial para o grafico

---
+++ lib/components/chart_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget{
+
+  final String label;
+  final double value;
+  final double percetage;
+
+  ChartBar({
+    required this.label,
+    required this.value,
+    required this.percetage,
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: <Widget>[
+        Text('R\$${value.toStringAsFixed(2)}'),
+        SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          child: null,
+        ),
+        SizedBox(height: 5),
+        Text(label)
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
@@ -47,10 +48,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>{
   final List<Transaction> _transactions = [
-    // Transaction(id: 't1', title: 'Novo Tênis de corrida', value: 310.76, date: DateTime.now()),
-    // Transaction(id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now())
+    Transaction(id: 't0', title: 'Conta antiga', value: 400.76, date: DateTime.now().subtract(Duration(days: 33))),
+    Transaction(id: 't1', title: 'Novo Tênis de corrida', value: 310.76, date: DateTime.now().subtract(Duration(days: 3))),
+    Transaction(id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now().subtract(Duration(days: 34)))
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((tr){
+      return tr.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ));
+    }).toList();
+  }
 
   _addTransaction(String title, double value){
     final newTransaction = Transaction(
@@ -91,13 +100,7 @@ class _MyHomePageState extends State<MyHomePage>{
             child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Gráfico'),
-                  elevation: 5,
-                ),
-              ),
+              Chart(_recentTransactions),
               TransactionList(_transactions),
 
             ],
