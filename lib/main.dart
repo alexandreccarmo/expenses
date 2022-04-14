a192ebd1713d7e830413aa73c1013bc322869d62
Alexandre C do Carmo <alexandrecdocarmo@Mac-mini-de-Alexandre-2.local>
2022-04-13 22:29:56 -0300

    adicionado recursos para usar cupertino em caso de iOS e material em caso de android e usar recursos especificos de cada plataforma

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,74 @@
-import 'package:expenses/components/chart.dart';
-import 'package:expenses/components/transaction_form.dart';
-import 'package:flutter/material.dart';
 import 'dart:math';
-import './components/transaction_form.dart';
-import './components/transaction_list.dart';
-import 'package:expenses/models/transaction.dart';
+import 'dart:io';
 
+import 'package:expenses/components/transaction_form.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-main()=>runApp(ExpensesApp());
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'components/chart.dart';
+import 'models/transaction.dart';
+
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-
   @override
-  Widget build(BuildContext context){
-      return MaterialApp(
-        home: MyHomePage(),
-          theme: ThemeData(
-            primarySwatch: Colors.purple,
-            accentColor: Colors.amber,
-            fontFamily: 'Quicksand',
-            textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: TextStyle(
-                fontFamily: 'OpenSans',
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-              button: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            appBarTheme: AppBarTheme(
-              textTheme: ThemeData.light().textTheme.copyWith(
-                headline6: TextStyle(
-                  fontFamily: 'OpenSans',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          headline6: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+          button: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            headline6: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
           ),
-      );
+        ),
+      ),
+    );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
-  // final List<Transaction> _transactions = [
-  //   Transaction(id: 't0', title: 'Conta antiga', value: 400.76, date: DateTime.now().subtract(Duration(days: 33))),
-  //   Transaction(id: 't1', title: 'Novo Tênis de corrida', value: 310.76, date: DateTime.now().subtract(Duration(days: 3))),
-  //   Transaction(id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now().subtract(Duration(days: 34))),
-  //   Transaction(id: 't3', title: 'Conta de agua', value: 250.30, date: DateTime.now().subtract(Duration(days: 34))),
-  //   Transaction(id: 't4', title: 'Conta de internet', value: 390.80, date: DateTime.now().subtract(Duration(days: 34))),
-  // ];
-
+class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
   bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
-    return _transactions.where((tr){
+    return _transactions.where((tr) {
       return tr.date.isAfter(DateTime.now().subtract(
         Duration(days: 7),
       ));
     }).toList();
   }
 
-  _addTransaction(String title, double value, DateTime date){
+  _addTransaction(String title, double value, DateTime date) {
     final newTransaction = Transaction(
-        id: Random().nextDouble().toString(),
-        title: title,
-        value: value,
-        date: date
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: date,
     );
 
     setState(() {
@@ -85,86 +78,113 @@ class _MyHomePageState extends State<MyHomePage>{
     Navigator.of(context).pop();
   }
 
-  _removeTransaction(String id){
+  _deleteTransaction(String id) {
     setState(() {
-      _transactions.removeWhere((tr){
-        return tr.id == id;
-      });
+      _transactions.removeWhere((tr) => tr.id == id);
     });
   }
 
   _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
-        context: context, 
-        builder: (_) {
-          return TransactionForm(_addTransaction);
-        });
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
+
+  Widget _getIconButton(IconData icon, Function() fn) {
+    return Platform.isIOS
+        ? GestureDetector(onTap: fn, child: Icon(icon))
+        : IconButton(icon: Icon(icon), onPressed: fn);
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    bool isLandSacape = mediaQuery.orientation == Orientation.landscape;
-    final appBar = AppBar(
-      title: Text('Despesas pessoais'),
-      actions: <Widget>[
-        if(isLandSacape)
-          IconButton(
-            icon: Icon(_showChart ? Icons.list : Icons.show_chart),
-            onPressed: () {
-              setState(() {
-                _showChart = !_showChart;
-              });
-            },
-          ),
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () => _openTransactionFormModal(context),
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
+
+    final actions = <Widget>[
+      if (isLandscape)
+        _getIconButton(
+          _showChart ? Icons.list : Icons.show_chart,
+              () {
+            setState(() {
+              _showChart = !_showChart;
+            });
+          },
         ),
-      ],
+      _getIconButton(
+        Platform.isIOS ? CupertinoIcons.add : Icons.add,
+            () => _openTransactionFormModal(context),
+      ),
+    ];
+
+    final PreferredSizeWidget appBar = AppBar(
+      title: Text('Despesas Pessoais'),
+      actions: actions,
     );
-    final availableHeight = mediaQuery.size.height - appBar.preferredSize.height - mediaQuery.padding.top;
 
-      return Scaffold(
-        appBar: appBar,
-        body: SingleChildScrollView(
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              // if(isLandSacape)
-              //   Row(
-              //     mainAxisAlignment: MainAxisAlignment.center,
-              //     children: <Widget>[
-              //       Text('Exibir gráfico'),
-              //       Switch(
-              //         value: _showChart,
-              //         onChanged: (value) {
-              //           setState(() {
-              //             _showChart = value;
-              //           });
-              //         },
-              //       ),
-              //     ],
-              //   ),
-              if (_showChart || !isLandSacape)
-                Container(
-                  height: availableHeight * (isLandSacape ? 0.8 : 0.3),
-                  child: Chart(_recentTransactions),
-                ),
-              if (!_showChart || !isLandSacape)
-                Container(
-                  height: availableHeight  * (isLandSacape ? 1 : 0.7),
-                  child: TransactionList(_transactions, _removeTransaction),
-                ),
-            ],
-          ),
+    final availableHeight = mediaQuery.size.height -
+        appBar.preferredSize.height -
+        mediaQuery.padding.top;
+
+    final bodyPage = SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // if (isLandscape)
+          //   Row(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: <Widget>[
+          //       Text('Exibir Gráfico'),
+          //       Switch.adaptive(
+          //         activeColor: Theme.of(context).accentColor,
+          //         value: _showChart,
+          //         onChanged: (value) {
+          //           setState(() {
+          //             _showChart = value;
+          //           });
+          //         },
+          //       ),
+          //     ],
+          //   ),
+          if (_showChart || !isLandscape)
+            Container(
+              height: availableHeight * (isLandscape ? 0.8 : 0.3),
+              child: Chart(_recentTransactions),
+            ),
+          if (!_showChart || !isLandscape)
+            Container(
+              height: availableHeight * (isLandscape ? 1 : 0.7),
+              child: TransactionList(_transactions, _deleteTransaction),
+            ),
+        ],
+      ),
+    );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Despesas Pessoais'),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: actions,
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => _openTransactionFormModal(context),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      );
+      ),
+      child: bodyPage,
+    )
+        : Scaffold(
+      appBar: appBar,
+      body: bodyPage,
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation:
+      FloatingActionButtonLocation.centerFloat,
+    );
   }
-
 }
