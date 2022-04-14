7db59ecd17f06fc5ef6573ed1c02189274905407
Alexandre C do Carmo <alexandrecdocarmo@Mac-mini-de-Alexandre-2.local>
2022-04-13 21:18:58 -0300

    adicionado recursos para deixar responsivo em modo retrato

--- lib/main.dart
+++ lib/main.dart
@@ -102,23 +102,31 @@ class _MyHomePageState extends State<MyHomePage>{
 
   @override
   Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Despesas pessoais'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () => _openTransactionFormModal(context),
-            ),
-          ],
+    final appBar = AppBar(
+      title: Text('Despesas pessoais'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _openTransactionFormModal(context),
         ),
+      ],
+    );
+    final availableHeight = MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+
+      return Scaffold(
+        appBar: appBar,
         body: SingleChildScrollView(
             child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Chart(_recentTransactions),
-              TransactionList(_transactions, _removeTransaction),
-
+              Container(
+                height: availableHeight * 0.3,
+                child: Chart(_recentTransactions),
+              ),
+              Container(
+                height: availableHeight * 0.7,
+                child: TransactionList(_transactions, _removeTransaction),
+              ),
             ],
           ),
         ),
